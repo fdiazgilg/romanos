dbe24a8023ede0d17795ebbbd8ece58e2739485f
fdiazgilg <fdiazgilg@gmail.com>
2019-11-12 22:33:03 +0100

    Traduccion a romanos hasta el infinito

--- conversion.py
+++ conversion.py
@@ -1,24 +1,52 @@
-def arabRomano(num):    
-    tipo1 = ('M','C','X','I')
-    tipo5 = ('D','L','V')
-    listRomano = []
+def arabRomano(num):
     
-    for i in range(-len(num), 0):
-        if num[i] <= '3':
-            listRomano.append(int(num[i]) * tipo1[i])
-        elif num[i] == '4':
-            listRomano.append(tipo1[i] + tipo5[i])
-        elif num[i] >= '5' and num[i] <= '8' :
-            listRomano.append(tipo5[i] + (int(num[i]) - 5) * tipo1[i])
-        elif num[i] == '9':
-            listRomano.append(tipo1[i] + tipo1[i-1])
+    _MAX = 3999
+    arabSep = []
+    
+    while int(num) > _MAX:
+        num1 = num[-3:]
+        arabSep.append(num1)        
+        num2 = num[:-3]
+        
+        num = num2
+    
+    arabSep.append(num)
+    arabSep.reverse()
+
+    listNumRomano = []
+    for numArab in arabSep:
+
+        tipo1 = ('M','C','X','I')
+        tipo5 = ('D','L','V')
+        listRomano = []
+        for j in range(-len(numArab), 0):
+            if numArab[j] <= '3':
+                listRomano.append(int(numArab[j]) * tipo1[j])
+            elif numArab[j] == '4':
+                listRomano.append(tipo1[j] + tipo5[j])
+            elif numArab[j] >= '5' and numArab[j] <= '8' :
+                listRomano.append(tipo5[j] + (int(numArab[j]) - 5) * tipo1[j])
+            elif numArab[j] == '9':
+                listRomano.append(tipo1[j] + tipo1[j-1])
+            else:
+                pass
+        
+        numRomano = ''
+        for k in range(len(listRomano)):
+            numRomano += listRomano[k]
+            
+        listNumRomano.append(numRomano)
+        
+        esRomano = ''
+        for l in range(len(listNumRomano)-1):
+            esRomano += listNumRomano[l]
+        
+        if len(listNumRomano) >= 2:
+            romano = '(' + esRomano + ')' + listNumRomano[-1]
         else:
-            pass
-    
-    numRomano = ''
-    for j in range(len(listRomano)):
-        numRomano += listRomano[j]
-    return numRomano
+            romano = esRomano + listNumRomano[-1]
+
+    return romano
 
 def romanoArab(num):
     dict = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
--- main.py
+++ main.py
@@ -17,26 +17,8 @@ def InputUntil(msg, validation = None, error_msg = 'Se ha producido un error', t
 
     return _input_value
 
-decimal = InputUntil("Introduzca un número entero y positivo: ", lambda x: esEntero(x) and int(x) > 0 and int(x) < 4000000, 'Debe introducir un valor entero, mayor que cero y menor que 4000000.\n', lambda x: x.lstrip('0'))
+decimal = InputUntil("Introduzca un número entero y positivo: ", lambda x: esEntero(x) and int(x) > 0, 'Debe introducir un valor entero y mayor que cero.\n', lambda x: x.lstrip('0'))
 
-if int(decimal) > 3999:
-    decimal1 = decimal[0:-3]
-    decimal2 = decimal[-3:]
-    roman1 = conversion.arabRomano(decimal1)
-    roman2 = conversion.arabRomano(decimal2)
-    
-    romano = '(' + roman1 + ')' + roman2
-    
-    romano1 = romano[1:romano.find(')')]
-    romano2 = romano[(romano.find(')'))+1:]
-    arab1 = conversion.romanoArab(romano1) * 1000
-    arab2 = conversion.romanoArab(romano2)
-
-    arabigo = arab1 + arab2
-    
-else:
-    romano = conversion.arabRomano(decimal)
-    arabigo = conversion.romanoArab(romano)
+romano = conversion.arabRomano(decimal)
 
 print("\nEl número romano correspondiente al número arábigo {} es {}.".format(int(decimal),romano))
-print("El número arábigo correspondiente al número romano {} es {}.".format(romano,arabigo))
